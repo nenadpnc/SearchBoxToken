7f0a76807344066e35a648a47705a72951e095ed
nenad <nenadpnc@gmail.com>
2014-07-27 21:17:58 +0200

    save

--- js/app.js
+++ js/app.js
@@ -23,12 +23,23 @@
         }
     ];
     
-    $('#search_box').tokenInput(
+    var searchBox = $('#search_box').tokenInput(
         categoryList,
         {
             theme: 'facebook',
             hintText: 'in <span>France</span>, landform <span>coast</span>, temperature <span>mild</span>, where I can <span>visit museums + go out</span><br> temperature <span>hot</span>, where I can <span>eat gourmet food</span>, related to <span>Harry Potter</span><br> where I can <span>do wine tasting</span>, in <span>Tuscany</span>, temperature <span>hot</span>',
             spinner: $('.spinner')
         }
-    )
+    );
+    
+    $('.search-btn-container').on('click', function(){
+        var tokens = searchBox.tokenInput('get');
+        var message = 'You selected: ';
+        for(var i = 0; i < tokens.length; i++){
+            message += tokens[i].category.name + ': ' + tokens[i].name + ', ';
+        }
+        alert(message);
+    });
+    
+    
 })();
